07f15b5856c5d3fb2d59bed0d033ca8ca83323f7
Manson <manson972@yahoo.com>
2025-05-25 19:03:01 +0800

    feat: add new pages, layouts, components and update app.vue

---
+++ components/app/navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav class="container">
+    <ul>
+      <li>
+        <NuxtLink to="/">
+          <strong>Test app</strong>
+        </NuxtLink>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <NuxtLink :to="{ name: 'create' }">Create Task</NuxtLink>
+      </li>
+    </ul>
+  </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <AppNavbar></AppNavbar>
+    <main class="container">
+      <slot />
+    </main>
+  </div>
+</template>
